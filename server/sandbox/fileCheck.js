bd7dd77ee0cde63d690aa6aba858d3a2f58e2805
Michael Soileau <webart.video@gmail.com>
2017-01-02 15:14:26 -0600

    Adding in test modules
    Signed-off-by: Michael Soileau <webart.video@gmail.com>

--- server/sandbox/fileCheck.js
+++ server/sandbox/fileCheck.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 /**
- * The fat arrow operation does not allow constructors or binding
+ * The fat arrow operation does not allow binding, it takes care of it automatically
  * @constructor
  */
 
@@ -45,6 +45,26 @@ let FileObj2 = function () {
     }
 };
 
+let FileObj3 = function () {
+    this.filename = '';
+    this.file_exists = (callback) => {
+        console.log("About to open " + this.filename);
+        fs.open(this.filename, 'r', (err, handle) => {
+            if (err) {
+                console.log("Cannot open: " + this.filename);
+                if (callback) {
+                    return callback(err);
+                } else {
+                    return null;
+                }
+            }
+
+            fs.close(handle);
+            return callback(null, true);
+        });
+    }
+};
+
 class FileObjClass {
 
     constructor(filename) {
@@ -71,8 +91,9 @@ class FileObjClass {
 
 let fo = new FileObj(),
     fo2 = new FileObj2(),
-    fo3 = new FileObjClass();
-fo3.filename = fo2.filename = fo.filename = 'This does not exist.';
+    fo3 = new FileObj3(),
+    foClass = new FileObjClass();
+foClass.filename = fo3.filename = fo2.filename = fo.filename = 'This does not exist.';
 
 let file_exists = (err, callback) => {
     if (err) {
@@ -88,3 +109,4 @@ let file_exists = (err, callback) => {
 fo.file_exists();
 fo2.file_exists();
 fo3.file_exists();
+foClass.file_exists();
