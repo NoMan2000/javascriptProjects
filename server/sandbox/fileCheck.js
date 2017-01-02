2738741565856f2b4156ebd4b3f282fc79e4e273
Michael Soileau <webart.video@gmail.com>
2017-01-02 10:00:43 -0600

    JavaScript this demonstration
    Demonstration of different ways of handling JavaScript by binding.
    
    Signed-off-by: Michael Soileau <webart.video@gmail.com>

---
+++ server/sandbox/fileCheck.js
@@ -0,0 +1,90 @@
+const fs = require('fs');
+
+/**
+ * The fat arrow operation does not allow constructors or binding
+ * @constructor
+ */
+
+let FileObj = function () {
+    this.filename = '';
+    this.file_exists = function (callback) {
+        console.log("About to open " + this.filename);
+        fs.open(this.filename, 'r', function (err, handle) {
+            if (err) {
+                console.log("Cannot open: " + this.filename);
+                if (callback) {
+                    return callback(err);
+                } else {
+                    return null;
+                }
+            }
+            fs.close(handle);
+            return callback(null, true);
+        }.bind(this));
+    }
+};
+
+let FileObj2 = function () {
+    let self = this;
+    self.filename = '';
+    self.file_exists = (callback) => {
+        console.log("About to open " + self.filename);
+        fs.open(self.filename, 'r', (err, handle) => {
+            if (err) {
+                console.log("Cannot open: " + self.filename);
+                if (callback) {
+                    return callback(err);
+                } else {
+                    return null;
+                }
+            }
+
+            fs.close(handle);
+            return callback(null, true);
+        });
+    }
+};
+
+class FileObjClass {
+
+    constructor(filename) {
+        this.filename = filename;
+    }
+
+    file_exists(callback) {
+        console.log("About to open " + this.filename);
+        fs.open(this.filename, 'r', (err, handle) => {
+            if (err) {
+                console.log("Cannot open: " + this.filename);
+                if (callback) {
+                    return callback(err);
+                } else {
+                    return null;
+                }
+            }
+
+            fs.close(handle);
+            return callback(null, true);
+        });
+    }
+}
+
+let fo = new FileObj(),
+    fo2 = new FileObj2(),
+    fo3 = new FileObjClass();
+fo3.filename = fo2.filename = fo.filename = 'This does not exist.';
+
+let file_exists = (err, callback) => {
+    if (err) {
+        console.log("error opening file: " + JSON.stringify(err));
+        return err;
+    }
+    if (callback) {
+        return callback();
+    }
+};
+
+
+fo.file_exists();
+fo2.file_exists();
+fo3.file_exists();
